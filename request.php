7cce30604423936a3a9f22d8c7ca17e5ed3274be
sourcecodescott <samath_scott@yahoo.com>
2015-11-15 00:36:12 -0500

    Update request.php

--- request.php
+++ request.php
@@ -53,47 +53,4 @@ else{
     print "<br>";
 }
 
-// header("Content-type: text/xml");
-// print("");
-
-
-// $string = <<<XML
-// <ol>
-//     <li>
-//         <definition>
-//             definition = a statement of the exact meaning of a word, especially in a dictionary.
-//         </definition>
-//         <example>
-//             example = the definition of bar is a place that sells drinks.
-//         </example>
-//         <author>
-//             author = Mos Def-inition
-//         </author>
-//     </li>
-//     <li>
-//         <definition>
-//             bar = a place that sells alcholic beverages.
-//         <example>
-//             example = we should go down to the bar for some drinks.
-//         </example>
-//         <author>
-//             author = Patrick O'Mally
-//         </author>
-//     </li>
-//     <li>
-//         <definition>
-//             ajax = technique which involves the use of javascript and xml.
-//         <example>
-//             example = i'm thinking of using ajax for a project.
-//         </example>
-//         <author>
-//             author = Jack Ace
-//         </author>
-//     </li>
-// </ol>
-// XML;
-
-// $xml = new SimpleXMLElement($string);
-// echo $xml->asXML();
-
 ?>
